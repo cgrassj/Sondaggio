03ad3a3674065c856771ecd2a97beb9a452a9fb6
GrassiC <claudio.grassi@sintel.net>
2018-07-16 08:29:44 +0200

    invio email wip

--- Sondaggio.Web/ControllerOdata/RisposteController.cs
+++ Sondaggio.Web/ControllerOdata/RisposteController.cs
@@ -13,6 +13,7 @@ using System.Web.Http.OData;
 using System.Web.Http.OData.Routing;
 using Questionario.Db;
 using Questionario.Db.Models;
+using System.Net.Mail;
 
 namespace Questionario.Web.Controller
 {
@@ -33,73 +34,90 @@ namespace Questionario.Web.Controller
 				return _db.Risposte;
 			}
 
-		// GET: odata/Risposte(5)
-		[EnableQuery]
-		[Route("odata/risposte/{id}")]
-		public SingleResult<Risposta> GetRisposta(int key)
-			{
-				return SingleResult.Create(_db.Risposte.Where(b => b.IdRisposta == key));
-			}
-
-		// PATCH: odata/Risposte(5)
-		[AcceptVerbs("PATCH", "MERGE")]
-			public async Task<IHttpActionResult> Patch(int key, Delta<Risposta> patch)
-			{
-				if (!ModelState.IsValid)
+			// GET: odata/Risposte(5)
+			[EnableQuery]
+			[Route("odata/risposte/{id}")]
+			public SingleResult<Risposta> GetRisposta(int key)
 				{
-					return BadRequest(ModelState);
+					return SingleResult.Create(_db.Risposte.Where(b => b.IdRisposta == key));
 				}
 
-				var risposta = await _db.Risposte.FindAsync(key);
-				if (risposta == null)
+			// PATCH: odata/Risposte(5)
+			[AcceptVerbs("PATCH", "MERGE")]
+				public async Task<IHttpActionResult> Patch(int key, Delta<Risposta> patch)
 				{
-					return NotFound();
+					if (!ModelState.IsValid)
+					{
+						return BadRequest(ModelState);
+					}
+
+					var risposta = await _db.Risposte.FindAsync(key);
+					if (risposta == null)
+					{
+						return NotFound();
+					}
+
+					patch.Patch(risposta);
+					_db.Entry(risposta).State = EntityState.Modified;
+
+					await _db.SaveChangesAsync();
+
+					return Updated(risposta);
 				}
 
-				patch.Patch(risposta);
-				_db.Entry(risposta).State = EntityState.Modified;
-
-				await _db.SaveChangesAsync();
-
-				return Updated(risposta);
-			}
-
-		// POST: odata/Risposte
-		public async Task<IHttpActionResult> Post(Risposta risposta)
-			{
-				if (!ModelState.IsValid)
+			// POST: odata/Risposte
+			public async Task<IHttpActionResult> Post(Risposta risposta)
 				{
-					return BadRequest(ModelState);
+					if (!ModelState.IsValid)
+					{
+						return BadRequest(ModelState);
+					}
+
+					_db.Risposte.Add(risposta);
+					await _db.SaveChangesAsync();
+
+					return Created(risposta);
 				}
 
-				_db.Risposte.Add(risposta);
-				await _db.SaveChangesAsync();
-
-				return Created(risposta);
-			}
-
-		// DELETE: odata/Risposte(5)
-		public async Task<IHttpActionResult> Delete(int key)
-			{
-				var entity = await _db.Risposte.FindAsync(key);
-				if (entity == null)
+			// DELETE: odata/Risposte(5)
+			public async Task<IHttpActionResult> Delete(int key)
 				{
-					return NotFound();
+					var entity = await _db.Risposte.FindAsync(key);
+					if (entity == null)
+					{
+						return NotFound();
+					}
+
+					_db.Risposte.Remove(entity);
+					await _db.SaveChangesAsync();
+
+					return StatusCode(HttpStatusCode.OK);
 				}
-
-				_db.Risposte.Remove(entity);
-				await _db.SaveChangesAsync();
-
-				return StatusCode(HttpStatusCode.OK);
-			}
 
 			protected override void Dispose(bool disposing)
-			{
-				if (disposing)
 				{
-					_db.Dispose();
+					if (disposing)
+					{
+						_db.Dispose();
+					}
+					base.Dispose(disposing);
 				}
-				base.Dispose(disposing);
+
+
+			public void SendMail(string cognomeNome, string sottoTitoloSondaggio, string descrizioneSondaggio)
+			{
+				MailMessage m = new MailMessage();
+				SmtpClient client = new SmtpClient();
+				client.Port = 25;
+				client.Host = "smtp.cgil.lombardia.it";
+				m.Subject = "oggetto email";
+				m.Body = TestoEmail.Replace("CognomeNome",).Replace("SottoTitoloSondaggio", sottoTitoloSondaggio).Replace("DescrizioneSondaggio", descrizioneSondaggio);
+				client.Send(m);
 			}
+
+		protected const string TestoEmail = "<div>Gentile {{CognomeNome}}</div><div>vorremmo ringraziarLa per averci scelto e vorremmo invitarLa a rispondere ad un breve sondaggio per aiutarci a migliorare il nostro servizio</div><div>&nbsp;</div><div>La preghiamo di accedere al seguente <a href='{{URL}}'>link</a></div><div>&nbsp;</div><div>&nbsp;</div><div>&nbsp;</div><div>&nbsp;</div><div><div>Cordialmente:</div>{{SottoTitoloSondaggio}} {{DescrizioneSondaggio}}</div>";
+
 		}
+
+		
 	}
