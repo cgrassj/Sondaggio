76ce860dbe22446d922f06d11b122a420ab97b6d
CGIL\CarminatiP <paolo.carminati@sintel.net>
2018-07-16 17:28:50 +0200

    Modifiche varie. Aggiunta funzione per validazione codice fiscale da mettere direttamente nella textbox e non sul save

--- Sondaggio.Web/app/main/utentiController.js
+++ Sondaggio.Web/app/main/utentiController.js
@@ -27,7 +27,24 @@
 					});
 				}
 				$scope.save = function (result) {
-					utentiService.save(result).then(function () {
+					utentiService.save(result).then(function (result) {
+						//Controllo da effettuare su nuovo, su salvataggio viene cambiata la chiave e deve essere messo in sola lettura
+						var cf = result.data.IdUtente.toUpperCase();
+						var cfReg = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/;
+						if (!cfReg.test(cf))
+							alert("Codice Fiscale errato");
+						var set1 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+						var set2 = "ABCDEFGHIJABCDEFGHIJKLMNOPQRSTUVWXYZ";
+						var setpari = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+						var setdisp = "BAKPLCQDREVOSFTGUHMINJWZYX";
+						var s = 0;
+						for (i = 1; i <= 13; i += 2)
+							s += setpari.indexOf(set2.charAt(set1.indexOf(cf.charAt(i))));
+						for (i = 0; i <= 14; i += 2)
+							s += setdisp.indexOf(set2.charAt(set1.indexOf(cf.charAt(i))));
+						if (s % 26 != cf.charCodeAt(15) - 'A'.charCodeAt(0))
+							alert("Codice Fiscale errato");
+
 					}).catch(function () {
 						$state.go("errore");
 					});
