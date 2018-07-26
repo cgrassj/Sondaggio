68461899ee4eef4f069ca1616c1e62612321ee9b
GrassiC <claudio.grassi@sintel.net>
2018-07-26 10:06:20 +0200

    Inizio gestione tre pagine per recensione

--- Sondaggio.Web/app/main/risposteController.js
+++ Sondaggio.Web/app/main/risposteController.js
@@ -1,120 +1,121 @@
 (function (window, angular) {
-	'use-strict';
-	angular.module('mainModule')
-		.config(function ($stateProvider) {
-			$stateProvider
-				.state('public.inizioRisposta',
-					{
-						url: '/dettagliRisposta/:id',
-						templateUrl: 'app/main/landing.html',
+  'use-strict';
+  angular.module('mainModule')
+    .config(function ($stateProvider) {
+      $stateProvider
+        .state('public.inizioRisposta',
+          {
+            url: '/dettagliRisposta/:id',
+            templateUrl: 'app/main/landing.html',
             controller: 'risposteInizioCtrl'
           })
         .state('public.inserisciRisposta',
           {
-            url: '/dettagliRisposta/',
+            url: '/inserisciRisposta/:id',
             templateUrl: 'app/main/main.html',
-            controller: 'risposteDettaglioCtrl'
+            controller: 'risposteDettaglioCtrl',
           })
         .state('public.fineRisposta',
           {
-            url: '/dettagliRisposta/',
+            url: '/fineRisposta',
             templateUrl: 'app/main/end.html',
             controller: 'risposteFineCtrl'
           })
-				.state('private.dettagliRisposta',
-					{
-						url: '/dettagliRisposta/:id',
-						templateUrl: 'app/main/main.html',
-						controller: 'risposteCtrl'
-					})
-				.state('private.risposte',
-					{
-						url: '/risposte',
-						templateUrl: 'app/main/domande.html',
-						controller: 'risposteCtrl'
-					});
-		})
-		.controller('risposteCtrl',
-			function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
+        .state('private.dettagliRisposta',
+          {
+            url: '/dettagliRisposta/:id',
+            templateUrl: 'app/main/main.html',
+            controller: 'risposteCtrl'
+          })
+        .state('private.risposte',
+          {
+            url: '/risposte',
+            templateUrl: 'app/main/domande.html',
+            controller: 'risposteCtrl'
+          });
+    })
+    .controller('risposteCtrl',
+      function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
 
         //popola lista sondaggi
-				sondaggiService.list().then(function (result) {
-					$scope.Sondaggi = result.data;
-				}).catch(function () {
-					$state.go("errore");
-				});
+        sondaggiService.list().then(function (result) {
+          $scope.Sondaggi = result.data;
+        }).catch(function () {
+          $state.go("errore");
+        });
 
-				if (typeof $stateParams.id === "undefined" || $stateParams.id === "") {
-					domandeService.list().then(function (result) {
-						$scope.ListaDomande = result.data;
-					}).catch(function () {
-						$state.go("errore");
-					});
-				} else {
-					domandeService.detail($stateParams.id).then(function (result) {
-						$scope.Domanda = result.data;
-					}).catch(function () {
-						$state.go("errore");
-					});
-				}
+        if (typeof $stateParams.id === "undefined" || $stateParams.id === "") {
+          domandeService.list().then(function (result) {
+            $scope.ListaDomande = result.data;
+          }).catch(function () {
+            $state.go("errore");
+          });
+        } else {
+          domandeService.detail($stateParams.id).then(function (result) {
+            $scope.Domanda = result.data;
+          }).catch(function () {
+            $state.go("errore");
+          });
+        }
 
         //popola lista Utenti
-				$scope.Utenti = null;
-				utentiService.list().then(function (result) {
-					$scope.Utenti = result.data;
-				}).catch(function () {
-					$state.go("errore");
-				});
+        $scope.Utenti = null;
+        utentiService.list().then(function (result) {
+          $scope.Utenti = result.data;
+        }).catch(function () {
+          $state.go("errore");
+        });
 
         //popola lista domande del sondaggio selezionato
-				$scope.updatedomande = function (sondaggio) {
-					domandeService.updateDomande(sondaggio.IdSondaggio).then(function (result) {
-						$scope.Domande = result.data;
-					});
-				}
+        $scope.updatedomande = function (sondaggio) {
+          domandeService.updateDomande(sondaggio.IdSondaggio).then(function (result) {
+            $scope.Domande = result.data;
+          });
+        }
 
         $scope.save = function () {
           risposteService.create($scope.selectedDomanda.IdDomanda, $scope.selectedUtente.IdUtente).then(function (result) {
-           
-            
 
-              window.alert("Mail inviata.");
+
+
+            window.alert("Mail inviata.");
 
           });
         }
-     })
+      })
     .controller('risposteDettaglioCtrl',
       function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
 
         $scope.nuovaRisposta = null;
-
-        if (typeof $scope.id === "undefined" || $scope.id === "") {
+        if (typeof $stateParams.id === "undefined" || $stateParams.id === "") {
           $state.go("errore");
         } else {
-          loadReview($scope.id)
+          loadReview($stateParams.id)
         }
         $scope.save = function () {
           if ($scope.rispostaDtAgg) {
             if (confirm("Sicuro di voler modificare la recensione?"))
               risposteService.save($scope.Risposta).then(function (result) {
-                //window.alert("Recensione modificata.");
                 $scope.nuovaRisposta = 1;
                 $state.go("public.fineRisposta");
               });
             else {
-              loadReview($scope.Risposta.IdRisposta);
-               
+              //loadReview($scope.Risposta.IdRisposta);
+              loadReview($scope.$id);
+
             }
           }
           else
             risposteService.save($scope.Risposta).then(function (result) {
-              //window.alert("Recensione pubblicata.");
               $scope.nuovaRisposta = 1;
               $state.go("public.fineRisposta");
             });
         }
 
-        $scope.annulla = function () { loadReview($scope.Risposta.IdRisposta); $scope.nuovaRisposta = null; }
+        $scope.annulla = function () {
+          loadReview($scope.Risposta.IdRisposta);
+          $scope.nuovaRisposta = null;
+        }
 
         function loadReview(idRisposta) {
           risposteService.detail(idRisposta).then(function (result) {
@@ -125,23 +126,20 @@
           });
         }
 
-    })
-  .controller('risposteInizioCtrl',
-    function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
-      if (typeof $stateParams.id === "undefined" || $stateParams.id === "") {
-        $state.go("errore");
-      } else {
-        //loadReview($stateParams.id)
-        $scope.id = $stateParams.id;
-      }
-
-      $scope.successivo = function () { $state.go("public.inserisciRisposta"); }
-    })
-  .controller('risposteFineCtrl',
-    function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
-       
-
-      $scope.successivo = function () { $state.go("public.inserisciRisposta"); }
-    });
+      })
+    .controller('risposteInizioCtrl',
+      function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
+        if (typeof $stateParams.id === "undefined" || $stateParams.id === "") {
+          $state.go("errore");
+        } else 
+          $scope.stateParams = $stateParams;
+        $scope.idRisposta = $stateParams.id;
+          $scope.state = $state;
+        $scope.successivo = function () { $state.go("public.inserisciRisposta", { 'IdRispostaDaCaricare': $stateParams.id }); }
+      })
+    .controller('risposteFineCtrl',
+      function ($scope, $state, $stateParams, domandeService, risposteService, sondaggiService, utentiService) {
+        $scope.successivo = function () { $state.go("public.inserisciRisposta"); }
+      });
 })(window, window.angular);
 
